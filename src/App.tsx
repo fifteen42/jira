e3b839f44c9005896ff945e19be58891c4f8277a
tingfei <1228959039@qq.com>
2022-03-29 08:47:52 +0800

    feat: 添加登录注册系统

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { LoginScreen } from "screens/login";
 import "./App.css";
-// import { ProjectListScreen } from "./screens/project-list";
-// import { TsReactTest } from "screens/project-list/try-use-array";
+import { useAuth } from "context/auth-context";
+import { AuthenticatedApp } from "authenticated-app";
+import { UnauthenticatedApp } from "unauthenticated-app";
 
 function App() {
+  const { user } = useAuth();
   return (
     <div className="App">
-      {/* <ProjectListScreen></ProjectListScreen> */}
-      {/* <TsReactTest></TsReactTest> */}
-      <LoginScreen />
+      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
     </div>
   );
 }
