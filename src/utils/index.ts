253a5a3990bc30a9b4bfb0ad4ffd8a707a40cb13
tingfei <1228959039@qq.com>
2022-04-03 10:06:49 +0800

    kanban和epic

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -68,3 +68,5 @@ export const useDocumentTitle = (title: string, keepOnUnmount = true) => {
     };
   }, [keepOnUnmount, oldTitle]);
 };
+
+export const resetRoute = () => (window.location.href = window.location.origin);
